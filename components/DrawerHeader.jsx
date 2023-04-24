ffd089fc4bcbefc824c40c8ee73c81abcd545715
zoeferencova <zoeferencova@gmail.com>
2023-04-24 13:53:43 -0400

    updated font smoothing

--- components/DrawerHeader.jsx
+++ components/DrawerHeader.jsx
@@ -6,7 +6,7 @@ export default function DrawerHeader({ title, subtitle, buttons, logo }) {
             <div className='flex items-center mb-8'>
                 <img className='h-[4.7rem] w-[4.7rem] mob:h-24 mob:w-24 mr-5 mob:mr-8 rounded-lg' src={logo} />
                 <div className=''>
-                    <h2 className='text-base mob:text-xl mb-[0.7rem] mob:mb-4 tracking-[0.01rem] mob:tracking-wide'>{title}</h2>
+                    <h2 className='no-smooth text-base mob:text-xl mb-[0.7rem] mob:mb-4 tracking-[0.01rem] mob:tracking-wide'>{title}</h2>
                     {subtitle && <p className='w-full mob:w-64 text-gray-400 -mt-2 leading-[1.2rem] mob:leading-[1.35rem] text-[0.85rem] mob:text-base'>{subtitle}</p>}
                     {!subtitle && buttons.map(button => button)}
                 </div>
--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -6,7 +6,7 @@ export default function ProjectCard({ project, openDrawer }) {
         <div className='flex justify-start col:justify-between items-center w-full cursor-pointer' onClick={() => openDrawer(project)}>
             <img src={project.logo.url} width='20' height='20' className='w-16 h-16 mob:w-20 mob:h-20 mr-4 mob:mr-6' />
             <div>
-                <h2 className='mb-1 mob:mb-1 mob:text-base text-[15px]'>{project.title}</h2>
+                <h2 className='no-smooth mb-1 mob:mb-1 mob:text-base text-[15px]'>{project.title}</h2>
                 <p className='text-[13px] mob:text-sm lg:text-[15px] text-gray-400 max-w-[13rem] col:max-w-full'>{project.subtitle}</p>
             </div>
             <Button text='View' extraClasses='col:ml-2 lg:ml-4 ml-auto' />
