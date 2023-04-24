4d6044ab588962096b4fdb6e0e40745c86c148c3
zoeferencova <zoeferencova@gmail.com>
2023-04-24 13:58:45 -0400

    updated favicon path

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps }) {
         <title>Zoe Ferencova</title>
         <meta name="description" content="Portfolio for Zoe Ferencova" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/favicon.ico" />
       </Head>
       <Layout>
         <Component {...pageProps} />
